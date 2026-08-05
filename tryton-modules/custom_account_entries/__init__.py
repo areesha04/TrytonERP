7bae14378348a5610be8c46be272a8a09e5c246e
Areesha Afzal <areeshaafzal99@gmail.com>
2026-08-05 16:13:55 +0500

    fix: separate fund transfer and bank/cash payment

--- tryton-modules/custom_account_entries/__init__.py
+++ tryton-modules/custom_account_entries/__init__.py
@@ -1,8 +1,10 @@
 from trytond.pool import Pool
-from .account import QuickAccountEntry, Account
+from .account import Account, QuickAccountEntry, MultiExpenseEntry, MultiExpenseEntryLine
 
 def register():
     Pool.register(
         Account,
         QuickAccountEntry,
+        MultiExpenseEntry,
+        MultiExpenseEntryLine,
         module='custom_account_entries', type_='model')
